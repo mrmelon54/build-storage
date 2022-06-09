5964af1aeffec0ef22ae791e7a7d11a444e766de
MrMelon <github@onpointcoding.net>
2022-06-09 12:00:16 +0100

    Start implementing login system for website

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
 	github.com/joho/godotenv v1.4.0
 	golang.org/x/oauth2 v0.0.0-20220524215830-622c5d57e401
 	gopkg.in/yaml.v3 v3.0.1
@@ -12,6 +13,7 @@ require (
 
 require (
 	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- web/web.go
+++ web/web.go
@@ -6,13 +6,15 @@ import (
 	"build-storage/utils"
 	_ "embed"
 	"encoding/gob"
-	"fmt"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
 
 var (
@@ -22,10 +24,6 @@ var (
 	groupTemplate string
 )
 
-type Module struct {
-	sessionWrapper func(cb func(http.ResponseWriter, *http.Request, *utils.State)) func(rw http.ResponseWriter, req *http.Request)
-	oauthClient    *oauth2.Config
-}
 type buildServiceKeyType int
 
 const (
@@ -36,19 +34,36 @@ const (
 	KeyRefreshToken
 )
 
-func SetupWebServer(configYml structure.ConfigYaml, buildManager *manager.BuildManager) *http.Server {
-	gob.Register(new(buildServiceKeyType))
-	oauthConfig := &oauth2.Config{
-		ClientID:     os.Getenv("MELON_CLIENT_ID"),
-		ClientSecret: os.Getenv("MELON_CLIENT_SECRET"),
-		Scopes:       []string{"openid"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://id.mrmelon54.xyz/oauth/authorize",
-			TokenURL: "https://id.mrmelon54.xyz/api/oauth/token",
+var sessionStore = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+type Module struct {
+	sessionStore *sessions.CookieStore
+	oauthConfig  *oauth2.Config
+	stateManager StateManager
+	configYml    structure.ConfigYaml
+	buildManager *manager.BuildManager
+}
+
+func New(configYml structure.ConfigYaml, buildManager *manager.BuildManager) *Module {
+	return &Module{
+		oauthConfig: &oauth2.Config{
+			ClientID:     os.Getenv("MELON_CLIENT_ID"),
+			ClientSecret: os.Getenv("MELON_CLIENT_SECRET"),
+			Scopes:       []string{"openid"},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  os.Getenv("MELON_AUTHORIZE_URL"),
+				TokenURL: os.Getenv("MELON_TOKEN_URL"),
+			},
+			RedirectURL: os.Getenv("MELON_REDIRECT_URL"),
 		},
-		RedirectURL: os.Getenv("MELON_REDIRECT_URL"),
+		stateManager: StateManager{&sync.RWMutex{}, make(map[uuid.UUID]*utils.State)},
+		configYml:    configYml,
+		buildManager: buildManager,
 	}
-	fmt.Println(oauthConfig.RedirectURL)
+}
+
+func SetupModle(configYml structure.ConfigYaml, buildManager *manager.BuildManager) *http.Server {
+	gob.Register(new(buildServiceKeyType))
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
@@ -70,6 +85,9 @@ func SetupWebServer(configYml structure.ConfigYaml, buildManager *manager.BuildM
 			http.Error(rw, "500 Internal Server Error", http.StatusInternalServerError)
 		}
 	}).Methods(http.MethodGet)
+	router.HandleFunc("/login", func(rw http.ResponseWriter, req *http.Request) {
+
+	})
 	router.HandleFunc("/{group}", func(rw http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 		group, ok := buildManager.GetGroup(vars["group"])
@@ -113,4 +131,33 @@ func fillTemplate(rw http.ResponseWriter, text string, data any) error {
 		return err
 	}
 	return parse.Execute(rw, data)
+}
+
+type StateManager struct {
+	MSync  *sync.RWMutex
+	States map[uuid.UUID]*utils.State
+}
+
+func (m *StateManager) sessionWrapper(cb func(http.ResponseWriter, *http.Request, *utils.State)) func(rw http.ResponseWriter, req *http.Request) {
+	return func(rw http.ResponseWriter, req *http.Request) {
+		session, _ := sessionStore.Get(req, "build-storage-session")
+		if a, ok := session.Values["session-key"]; ok {
+			if b, ok := a.(uuid.UUID); ok {
+				m.MSync.RLock()
+				c, ok := m.States[b]
+				m.MSync.RUnlock()
+				if ok {
+					cb(rw, req, c)
+					return
+				}
+			}
+		}
+		u := utils.NewState()
+		m.MSync.Lock()
+		m.States[u.Uuid] = u
+		m.MSync.Unlock()
+		session.Values["session-key"] = u.Uuid
+		_ = session.Save(req, rw)
+		cb(rw, req, u)
+	}
 }
